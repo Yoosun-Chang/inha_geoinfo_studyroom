8eed147c834a867115e18cc16326b6df6f920702
hyori Choi <hyoring030@gmail.com>
2023-09-13 18:10:20 +0900

    login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Reservation from "./page/reservation";
-import Infrom from "./component/main/inform";
+import Inform from "./component/main/Inform";
 import Confirm from "./component/reservation/confirm";
 import Admin from "./page/admin";
 import Myreservation from "./page/myreservation";
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/main" element={<Infrom />} />
+        <Route path="/main" element={<Inform />} />
         <Route path="/reservation" element={<Reservation />} />
         <Route path="/confirm" element={<Confirm />} />
         <Route path="/admin" element={<Admin />} />
