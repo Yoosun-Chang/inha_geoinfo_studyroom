78a8f7413019e236c85361849290f586e2a0a8e1
hyori Choi <hyoring030@gmail.com>
2023-09-13 18:40:08 +0900

    도메인

--- src/component/reservation/confirmbtn.jsx
+++ src/component/reservation/confirmbtn.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const PopupContainer = styled.div`
   position: fixed;
@@ -30,7 +30,7 @@ const ConfirmButtonContainer = styled.div`
   height: 50px;
   flex-shrink: 0;
   border-radius: 50px;
-  background: #0089FF;
+  background: #0089ff;
   cursor: pointer;
   color: white;
   font-size: 16px;
@@ -59,7 +59,7 @@ function ConfirmButton() {
   const [showPopup, setShowPopup] = useState(false);
   const [agreed, setAgreed] = useState(false);
   const navigate = useNavigate();
-  
+
   const togglePopup = () => {
     setShowPopup(!showPopup);
   };
@@ -68,10 +68,10 @@ function ConfirmButton() {
     setAgreed(!agreed);
     if (!agreed) {
       // 동의합니다 체크박스가 체크되었을 때 /confirm 경로로 이동
-      navigate('/confirm');
+      navigate("/confirm");
     }
   };
-  
+
   return (
     <>
       <CenteredContainer>
@@ -83,12 +83,25 @@ function ConfirmButton() {
         <PopupContainer>
           <CenteredContainer>
             <h2>이용 규칙</h2>
-            <p>1. 1명 또는 2명 이용을 제한하지 않습니다. (줌 회의, 면접 준비 등) 1명 또는 2명 이용자는 예약 없이 빈 시간대에 이용해주세요. 대신, 예약이 발생하면 바로 비켜주세요.</p>
-            <p>2. 음료수, 간단한 다과의 취식은 허용합니다. 그러나 냄새가 나는 식사류의 취식은 제한합니다.  환기가 어려운 구조입니다.</p>
+            <p>
+              1. 1명 또는 2명 이용을 제한하지 않습니다. (줌 회의, 면접 준비 등)
+              1명 또는 2명 이용자는 예약 없이 빈 시간대에 이용해주세요. 대신,
+              예약이 발생하면 바로 비켜주세요.
+            </p>
+            <p>
+              2. 음료수, 간단한 다과의 취식은 허용합니다. 그러나 냄새가 나는
+              식사류의 취식은 제한합니다. 환기가 어려운 구조입니다.
+            </p>
             <p>3. 쓰레기와 지우개가루, 칠판을 정리한 후 퇴실해주세요.</p>
-            <p>4. 칠판 또는 책상을 닦을 때는 입구에 비치된 알코올을 이용해주세요.</p>
+            <p>
+              4. 칠판 또는 책상을 닦을 때는 입구에 비치된 알코올을 이용해주세요.
+            </p>
             <CheckboxLabel>
-              <CheckboxInput type="checkbox" checked={agreed} onChange={handleAgree} />
+              <CheckboxInput
+                type="checkbox"
+                checked={agreed}
+                onChange={handleAgree}
+              />
               동의합니다
             </CheckboxLabel>
           </CenteredContainer>
