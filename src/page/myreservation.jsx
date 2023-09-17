c10915a17608cc978fef8724624a5ce1bc9242f5
Yoosun-Jang <ceusun0815@naver.com>
2023-09-17 17:03:12 +0900

    예약 취소, 예약 목록 연결 완

--- src/page/myreservation.jsx
+++ src/page/myreservation.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import Top2 from "../component/top2";
 import List from "../component/myreservation/list";
-import Wave from "../component/Wave"
+import Wave from "../component/Wave";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const WaveContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: flex-end; /* Wave 컴포넌트를 오른쪽에 정렬합니다. */
+  align-items: flex-end; /* Wave 컴포넌트를 아래에 정렬합니다. */
+  position: relative; /* 위치를 상대적으로 설정합니다. */
+`;
 
 function Myreservation() {
-    return (
-        <div>
-            <Top2/>
-            <List />
-            
-            <List />
-            <List />
-            <List />
-            <List />
-            <Wave/>
-        </div>
-    );
+  return (
+    <Container>
+      <Top2 />
+      <List />
+      <WaveContainer>
+        <Wave />
+      </WaveContainer>
+    </Container>
+  );
 }
 
 export default Myreservation;
