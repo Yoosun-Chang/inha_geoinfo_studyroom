f47dc8d7fcb98196ffcc4985c354cd7a2512bba1
Yoosun-Chang <12201321@inha.edu>
2023-09-22 14:16:31 +0900

    feat: 로컬에 로그인 정보 없을 시 메인으로 이동

--- src/page/admin.jsx
+++ src/page/admin.jsx
@@ -1,22 +1,35 @@
-import React from "react";
-import Calendar from "../component/admin/calendar"
-import Roombtn from "../component/admin/roombtn"
+import React, { useEffect } from "react";
+import Calendar from "../component/admin/calendar";
+import Roombtn from "../component/admin/roombtn";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   text-align: center;
 `;
 
-
 function Admin() {
-    return (
-        <div>
-            <Calendar/>
-            <Container>
-            <Roombtn/>
-            </Container>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  // 페이지 로딩 시 로컬 스토리지에서 schoolNumber와 name 확인
+  useEffect(() => {
+    const schoolNumber = localStorage.getItem("schoolNumber");
+    const name = localStorage.getItem("name");
+
+    // schoolNumber와 name이 없을 때 /main 페이지로 이동
+    if (!schoolNumber || !name) {
+      navigate("/main");
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <Calendar />
+      <Container>
+        <Roombtn />
+      </Container>
+    </div>
+  );
 }
 
 export default Admin;
